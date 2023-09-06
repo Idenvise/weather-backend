c6261417997945e3e3fda5962b2096955be2014e
Pavel Zavezenov <pavel290430@yandex.ru>
2023-09-06 15:13:45 +0700

    fix

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,14 +1,5 @@
 const options = {
   origin: [
-    'https://frontend.mesto.nomorepartiesxyz.ru',
-    'https://Idenvise.github.io',
-    'http://frontend.mesto.nomorepartiesxyz.ru/',
-    'https://web.postman.co/',
-    'https://movies.nomoredomains.icu/',
-    'https://localhost:3000',
-    'http://movies.nomoredomains.icu/',
-    'https://movies.nomoredomains.icu',
-    'http://movies.nomoredomains.icu',
     'http://localhost:5173',
   ],
   methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
